228d0afa75e487cc650ce10a09fd17b6f94311ed
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-06 16:45:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,11 +43,10 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "SELECT
-  FROM
-  JOIN
-  ON  =
-  GROUP BY
-  ORDER BY
-  "
+  "SELECT projects.category, SUM(pledges.amounts)
+  FROM projects
+  JOIN pledges
+  ON projects.id = pledges.product_id
+  GROUP BY projects.category
+  HAVING projects.category = 'books'"
 end
