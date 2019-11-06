719fef852532b591b16abf1b347e662a50a63fc4
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-06 16:28:45 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,8 +31,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   JOIN pledges
   ON  = users.id = pledges.user.id
   GROUP BY users.name
-  ORDER BY summed_amount
-  "
+  ORDER BY summed_amount"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
