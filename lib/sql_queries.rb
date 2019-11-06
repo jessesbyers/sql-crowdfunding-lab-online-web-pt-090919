8d738c3a6fe3a6356c6995eb6a53e2541941510d
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-06 16:08:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,17 +17,41 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  "SELECT
+  FROM
+  JOIN
+  ON  =
+  GROUP BY
+  ORDER BY
+  "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  "SELECT
+  FROM
+  JOIN
+  ON  =
+  GROUP BY
+  ORDER BY
+  "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  "SELECT
+  FROM
+  JOIN
+  ON  =
+  GROUP BY
+  ORDER BY
+  "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
+  "SELECT
+  FROM
+  JOIN
+  ON  =
+  GROUP BY 
+  ORDER BY
+  "
 end
