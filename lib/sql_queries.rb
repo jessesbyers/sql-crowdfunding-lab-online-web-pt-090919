fd8afeb368789ae46ef858eb87c8e1f11a584407
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-06 16:16:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,11 +17,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount - projects.funding_goal)
+  "SELECT projects.title, SUM(pledges.amount - projects.funding_goal) AS amount_over_goal
   FROM projects
   JOIN pledges
   ON projects.id = pledges.project_id
-  WHERE SUM(pledges.amount - projects.funding_goal) > 0
+  WHERE amount_over_goal > 0
   "
   # GROUP BY
   # ORDER BY
